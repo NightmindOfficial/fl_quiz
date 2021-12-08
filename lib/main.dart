5f754fd4cd98c5ccb78aa021007b6d6dce6e311b
NightmindOfficial <mail@otismohr.de>
2021-12-08 01:34:37 +0100

    Video 26

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,20 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   // Decorator - macht den Code klarer. Es gibt eine Build
-  // Methode, die wir selbst überschreiben wollen. 
+  // Methode, die wir selbst überschreiben wollen.
   // Damit sagen wir explizit, dass wir verstanden haben,
   // dass wir eine bestehende Methode überschreiben.
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Hallo Welt!"),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Meine erste App'),
+        ),
+        body: Text('Dies ist ein Standardtext.'),
+      ),
+    );
   }
 }
